aea1027cf6c9a8e2d0192ca1c63abe1426cfac97
Rafaela Oliveira Ribeiro <rafaela.o.ribeiro@hotmail.com>
2025-07-04 14:46:20 -0300

    fix: trocando para BrouwserRouter para exibição local

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TerminalPoem from './TerminalPoem';
 import SobreMim from './SobreMim';
 import MotivacaoStellantis from './Motivacao';
@@ -8,12 +8,12 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<TerminalPoem />} />
         <Route path="/sobre" element={<SobreMim />} />
         <Route path="/stellantis" element={<MotivacaoStellantis />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
