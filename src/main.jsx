a460035116d881e6f5a4f655a0c6d3d15bf3a8b0
Rafaela Oliveira Ribeiro <rafaela.o.ribeiro@hotmail.com>
2025-07-04 14:39:08 -0300

    fix: corrigindo para rodar localmente

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import TerminalPoem from './TerminalPoem';
 import SobreMim from './SobreMim';
 import MotivacaoStellantis from './Motivacao';
@@ -8,12 +8,12 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<TerminalPoem />} />
         <Route path="/sobre" element={<SobreMim />} />
         <Route path="/stellantis" element={<MotivacaoStellantis />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
